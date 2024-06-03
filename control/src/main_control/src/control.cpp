b7a80529fc705f78286a95c3fb89c5f8717f282a
InTwenty <esak703@gmail.com>
2024-06-03 18:05:52 +0900

    control update

--- control/src/main_control/src/control.cpp
+++ control/src/main_control/src/control.cpp
@@ -2,9 +2,13 @@
 #include <ros/ros.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Float32MultiArray.h>
-//#include <autoku_msgs/Gnss.h>
+#include "main_control/Gnss.h"
 #include <vector>
 #include <cmath>
+#include <lanelet2_core/primitives/Point.h>
+#include <lanelet2_projection/UTM.h>
+
+using namespace lanelet;
 double t_x,t_y;
 double gps_x,gps_y;
 double L = 0.8 ; //wheelbase
@@ -12,29 +16,39 @@ double yaw;
 int zone;
 bool northp;
 double target_steering;
+lanelet::projection::UtmProjector projector(lanelet::Origin({37.541759, 127.078433}));
+lanelet::GPSPoint pathPoint;
+lanelet::GPSPoint gpsPoint;
+lanelet::BasicPoint3d pathPoint_utm;
+lanelet::BasicPoint3d gpsPoint_utm;
+
+
 
 void point_CallBack(const std_msgs::Float32MultiArray::ConstPtr& msg){
   
-  double t_x = msg->data[0];
-  double t_y = msg->data[1];
-  // double temp_long = msg->data[0];
-  // double temp_lati = msg->data[1];
-  // GeographicLib::UTMUPS::Forward(temp_long, temp_lati, zone, northp, t_x, t_y);
+  pathPoint.lon = msg->data[0];
+  pathPoint.lat = msg->data[1];
+  pathPoint_utm = projector.forward(pathPoint);
+  t_x = pathPoint_utm.x();
+  t_y = pathPoint_utm.y();
+  std::cout<<"tx:" << t_x << "ty:" << t_y <<std::endl;
 }
-//const autoku_msgs::Gnss::ConstPtr& msg
-void gps_CallBack(const std_msgs::Float32MultiArray::ConstPtr& msg){
+
+void gps_CallBack(const main_control::Gnss::ConstPtr& msg){
   {
-    double gps_x = msg->data[0];//x
-    double gps_y = msg->data[1];//y
-    // double temp_long = msg->data[0];//longitude;
-    // double temp_lati = msg->data[1];//latitude;
-    // GeographicLib::UTMUPS::Forward(temp_long, temp_lati, zone, northp, gps_x, gps_y);
-    double heading = msg->data[2] + M_PI;//heading; // 0 ~ 2PI
+    gpsPoint.lon = msg->longitude;//x
+    gpsPoint.lat = msg->latitude;//y
+    gpsPoint_utm = projector.forward(gpsPoint);
+    gps_x = gpsPoint_utm.x();
+    gps_y = gpsPoint_utm.y();
+
+
+    double heading = msg->heading;//heading; // -PI ~ PI
     double dx = t_x - gps_x;
     double dy = t_y - gps_y;
     double distance_to_target = sqrt(dx * dx + dy * dy);
 
-    double angle_to_target = atan2(dy, dx) + M_PI; // 0 ~ 2PI 
+    double angle_to_target = atan2(dy, dx) ; // -PI ~ PI 
     double alpha = heading - angle_to_target ; //Heading 기준과 angle_to_target 기준을 맞춰야함 
 
     target_steering = atan2(2 * L * sin(alpha), distance_to_target);
@@ -53,11 +67,17 @@ int main(int argc, char** argv) {
   std_msgs::Float32 angle_msg;
   std_msgs::Float32 speed_msg;
   
+
+
+  
   while(ros::ok()){
     angle_msg.data = target_steering;
-    speed_msg.data = 5;
+    speed_msg.data = 0;
     target_angle_pub.publish(angle_msg);
     target_speed_pub.publish(speed_msg);
+    std::cout << "UTM X: " << t_x<< ", UTM Y: " << t_y << std::endl;
+    std::cout << "GPS X: " << gps_x << ", GPS Y: " << gps_y << std::endl;
+    std::cout << "Target_Steering:" << target_steering << std::endl;
     ros::spinOnce();
   }
   
